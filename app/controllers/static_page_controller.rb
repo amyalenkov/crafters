db97f47d9295f8c085e0faa9f8b91a1f4058bdea
amyalenkov <stmy999@gmail.com>
2015-11-23 15:01:58 +0300

    add checkboxes in search result.

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -3,8 +3,20 @@ class StaticPageController < ApplicationController
   end
 
   def search
+    @search_input = params[:search]
     @search_crafters  = Crafter.search params[:search], :star => true, :with => {:check => true},
                                        :page => params[:page], :per_page => 42
+    @cities = Array.new
+    @search_subcategories = Hash.new
+    @search_categories = Hash.new
+    @search_crafters.each { |crafter|
+      @search_subcategories[crafter.subcategory.id] = crafter.subcategory.name
+      @search_categories[crafter.subcategory.category.id] = crafter.subcategory.category.name
+      @cities.push crafter.city
+    }
+    @search_subcategories.keys.uniq!
+    @search_categories.keys.uniq!
+    @cities.uniq!
   end
 
   def search_ajax
