b285665e4cfb52f41bd96c89f822433e5f550952
amyalenkov <stmy999@gmail.com>
2015-12-24 14:14:48 +0300

    conversations: add send message from crafter to user.

--- app/controllers/conversation_controller.rb
+++ app/controllers/conversation_controller.rb
@@ -15,7 +15,12 @@ class ConversationController < ApplicationController
   end
 
   def index
-    @conversations = Conversation.where sender_user_id: current_user.id
+    unless current_user.nil?
+      @conversations = Conversation.where sender_user_id: current_user.id
+    end
+    unless current_crafter.nil?
+      @conversations = Conversation.where receiver_crafter_id: current_crafter.id
+    end
   end
 
   def show
@@ -27,9 +32,15 @@ class ConversationController < ApplicationController
     conversation = Conversation.find_by_id params[:conversation_id]
     message = conversation.messages.build
     message.body = params[:message]
-    message.sender_user_id = params[:sender_user_id]
-    message.receiver_crafter_id = params[:receiver_crafter_id]
     message.conversation_id = params[:conversation_id]
+    unless current_user.nil?
+      message.sender_user_id = params[:conversation_user_id]
+      message.receiver_crafter_id = params[:conversation_crafter_id]
+    end
+    unless current_crafter.nil?
+      message.receiver_user_id = params[:conversation_user_id]
+      message.sender_crafter_id = params[:conversation_crafter_id]
+    end
     message.save!
     render :nothing => "sdf"
   end
